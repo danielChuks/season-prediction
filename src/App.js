d4047392278eb467d92a63d0896ab06586c32b92
danielChuks <danieluchechukwuchuks1@gmail.com>
2022-04-27 20:42:39 +0000

    geolocation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
-import Comment from './component/Comment';
 
+//building an application that tell the weather.
 
-
+// getting a users physical location using geolocation....
 
 const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    postion => console.log(postion),
+    err => console.log(err)
+  )
   return (
     <>
         <div> 
-          <Comment  author="Desmond "  />
-          <Comment  author="Victor  " />
+            <h1> Hello there </h1>
         </div>
     </>
   );
---
+++ src/component/SeasonDisplay.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SeasonDisplay = () => {
+    return (
+        <div>
+            <h1> Season Display </h1>
+        </div>
+    )
+}
+
+
+export default SeasonDisplay;
